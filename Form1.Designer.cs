ec83e9f25e4a25fda5ab70c2dd0ebc601441be4c
student <student@PC-217>
2017-05-25 18:49:59 +0200

    skaner portów zrobiony

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(451, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(451, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets; 
+using System.Net; 
+
 
 namespace skaner_portów
 {
@@ -15,6 +18,25 @@ namespace skaner_portów
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            short[] ListaPortow = { 20, 21, 22, 80, 111, 30000 };
+            string host = textBox1.Text;
+            listBox1.Items.Add("Skanowanie portów dla " + host);
+            listBox1.Items.Add("To może chwilę potrwać ..."); 
+            foreach (short port in ListaPortow)
+            {
+                this.Refresh();
+                try { TcpClient klient = new TcpClient(host, port);
+                    listBox1.Items.Add("Port: " + port.ToString() + " jest otwarty");
+                }
+                catch
+                {
+                    listBox1.Items.Add("Port: " + port.ToString() + " jest zamknięty"); 
+                }
+            }
         }
     }
 }
